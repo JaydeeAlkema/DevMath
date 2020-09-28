3abe8e6dfb86486bb638db6c47ebc84a30e3d10c
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-09-28 14:22:19 +0200

    Write Vector2 functionality. Refactor code
    TODO:
    Write DirectionFromAngle()

--- DevMath/Vector2.cs
+++ DevMath/Vector2.cs
@@ -1,75 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevMath
 {
-    public struct Vector2
-    {
-        public float x;
-        public float y;
+	public struct Vector2
+	{
+		public float x;
+		public float y;
 
-        public float Magnitude
-        {
-            get { throw new NotImplementedException(); }
-        }
+		public float Magnitude
+		{
+			get { return (x * x + y * y); }
+		}
 
-        public Vector2 Normalized
-        {
-            get { throw new NotImplementedException(); }
-        }
+		public Vector2 Normalized
+		{
+			get { return new Vector2(x, y) / Magnitude; }
+		}
 
-        public Vector2(float x, float y)
-        {
-            this.x = x;
-            this.y = y;
-        }
+		public Vector2(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
 
-        public static float Dot(Vector2 lhs, Vector2 rhs)
-        {
-            throw new NotImplementedException();
-        }
+		public static float Dot(Vector2 lhs, Vector2 rhs)
+		{
+			return (lhs.x * rhs.x) + (lhs.y * rhs.y);
+		}
 
-        public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
-        {
-            throw new NotImplementedException();
-        }
+		public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
+		{
+			return new Vector2(a.x + t * (b.x - a.x), a.y + t * (b.y - a.y));
+		}
 
-        public static float Angle(Vector2 lhs, Vector2 rhs)
-        {
-            throw new NotImplementedException();
-        }
+		public static float Angle(Vector2 lhs, Vector2 rhs)
+		{
+			Vector2 delta = new Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
 
-        public static Vector2 DirectionFromAngle(float angle)
-        {
-            throw new NotImplementedException();
-        }
+			double angleRadians = Math.Atan2(delta.y, delta.x);
 
-        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
-        {
-            throw new NotImplementedException();
-        }
+			double angleDegrees = angleRadians * DevMath.RadToDeg((float)angleRadians);
 
-        public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
-        {
-            throw new NotImplementedException();
-        }
+			return (float)angleDegrees;
+		}
 
-        public static Vector2 operator -(Vector2 v)
-        {
-            throw new NotImplementedException();
-        }
+		public static Vector2 DirectionFromAngle(float angle)
+		{
+			throw new NotImplementedException();
+		}
 
-        public static Vector2 operator *(Vector2 lhs, float scalar)
-        {
-            throw new NotImplementedException();
-        }
+		public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+		{
+			return new Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
+		}
 
-        public static Vector2 operator /(Vector2 lhs, float scalar)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+		{
+			return new Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+		}
+
+		public static Vector2 operator -(Vector2 v)
+		{
+			return new Vector2(-v.x, -v.y);
+		}
+
+		public static Vector2 operator *(Vector2 lhs, float scalar)
+		{
+			return new Vector2(lhs.x * scalar, lhs.y * scalar);
+		}
+
+		public static Vector2 operator /(Vector2 lhs, float scalar)
+		{
+			return new Vector2(lhs.x / scalar, lhs.y / scalar);
+		}
+	}
 }
