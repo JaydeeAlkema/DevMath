58c08f9e5f25d98ec0f827bfa68d671588a7465e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-09-28 14:21:16 +0200

    Add RadToDeg and DegToRad functionality. Refactor code

--- DevMath/DevMath.cs
+++ DevMath/DevMath.cs
@@ -6,31 +6,31 @@ using System.Threading.Tasks;
 
 namespace DevMath
 {
-    public class DevMath
-    {
-        public static float Lerp(float a, float b, float t)
-        {
-            throw new NotImplementedException();
-        }
+	public class DevMath
+	{
+		public static float Lerp(float a, float b, float t)
+		{
+			throw new NotImplementedException();
+		}
 
-        public static float DistanceTraveled(float startVelocity, float acceleration, float time)
-        {
-            throw new NotImplementedException();
-        }
+		public static float DistanceTraveled(float startVelocity, float acceleration, float time)
+		{
+			throw new NotImplementedException();
+		}
 
-        public static float Clamp(float value, float min, float max)
-        {
-            throw new NotImplementedException();
-        }
+		public static float Clamp(float value, float min, float max)
+		{
+			throw new NotImplementedException();
+		}
 
-        public static float RadToDeg(float angle)
-        {
-            throw new NotImplementedException();
-        }
+		public static float RadToDeg(float angle)
+		{
+			return (float)(angle * 180 / Math.PI);
+		}
 
-        public static float DegToRad(float angle)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public static float DegToRad(float angle)
+		{
+			return (float)(angle * Math.PI / 180);
+		}
+	}
 }
