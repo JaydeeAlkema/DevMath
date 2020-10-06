706268f95383faa3ac302e195f5e5de509758f62
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-10-06 15:07:53 +0200

    Add Circle Math

--- DevMath/Circle.cs
+++ DevMath/Circle.cs
@@ -6,21 +6,32 @@ using System.Threading.Tasks;
 
 namespace DevMath
 {
-    public class Circle
-    {
-        public Vector2 Position
-        {
-            get; set;
-        }
+	public class Circle
+	{
+		public Vector2 Position
+		{
+			get; set;
+		}
 
-        public float Radius
-        {
-            get; set;
-        }
+		public float Radius
+		{
+			get; set;
+		}
 
-        public bool CollidesWith(Circle circle)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public bool CollidesWith(Circle circle)
+		{
+			float diffx = this.Position.x - circle.Position.x;
+			float diffy = this.Position.y - circle.Position.y;
+			double distance = Math.Sqrt(diffx * diffx + diffy * diffy);
+
+			if(distance < Radius + circle.Radius)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 }
